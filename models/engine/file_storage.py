5ac79a83dd47fe75a0584de53b25df9d68ecf0b1
betomukii <mukiibeatrice4@gmail.com>
2024-04-15 13:26:01 +0300

    task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        cls_name = cls.__name__
+        filtered = {}
+        for key in self.__objects.key():
+            if key.split('.')[0] == cls_name:
+                filtered[key] = self.__objects[key]
+        return filtered
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -18,9 +25,9 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(self.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
+            temp.update(self.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
@@ -42,9 +49,25 @@ class FileStorage:
                   }
         try:
             temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(self.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+        Deletes objects from the attribute __objects.
+        """
+        if obj is None:
+            pass
+        if obj in self.__objects:
+            del self.__objects[obj]
+            self.save()
+
+    def close(self):
+        """
+        Dispose of current session if active
+        """
+        self.session.remove()

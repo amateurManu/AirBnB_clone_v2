fc51af34a290574114ea7d7f8df1267c66ccf650
amateurManu <oderoemmanuel6@gmail.com>
2024-06-09 08:09:31 -0700

    added script that starts a Flask web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-""" This module serves a script for starting a live web server """
-
-
-from flask import Flask
+""" Script that starts a web application """
 
 app = Flask(__name__)
-""" Instatiating the Flask class """
-
 
 @app.route("/", strict_slashes=False)
-def hello_route():
-    """ Prints a message """
-    return "<p>Hello HBNB!<p>"
+def hello():
+    """ Displays a message """
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)

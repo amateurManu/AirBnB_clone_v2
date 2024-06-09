e9ee4d466a83fef34b9e83ee6d0af302535896a7
amateurManu <oderoemmanuel6@gmail.com>
2024-06-09 05:17:39 -0700

    added documentation and user execution right

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python3
+""" This module serves a script for starting a live web server """
+
+
 from flask import Flask
 
 app = Flask(__name__)
+""" Instatiating the Flask class """
+
 
 @app.route("/", strict_slashes=False)
 def hello_route():
+    """ Prints a message """
     return "<p>Hello HBNB!<p>"

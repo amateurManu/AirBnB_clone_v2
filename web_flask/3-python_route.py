16376fe97ac93579963a7a4dd91fd421419ad78b
amateurManu <oderoemmanuel6@gmail.com>
2024-06-09 21:08:03 -0700

    script with the /python/<text> route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Displays a message """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Displays a message """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text():
+    """ Displays 'C' followed by text variable
+        Underscore symbols are replaced with a space
+    """
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text='is cool'):
+    """ Displays 'Python' followed by value of <text> """
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
+    return "Python %s" % text
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

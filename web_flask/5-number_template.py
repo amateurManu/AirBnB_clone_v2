cfdb9876243266550f5f92c46e4ea3371c94f4a3
Emmanuel Odero <oderoemmanuel6@gmail.com>
2024-07-12 08:19:53 +0000

    added parameter to clear error on strict_slashes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def number(n):
     return "%d is a number" % n
 
 
-@app.route("/number_template/<int:n>", strict_flashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """ Displays a number """
     return render_template("5-number.html", number=n)
